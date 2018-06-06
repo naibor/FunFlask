46d1026e6422ba11f8e69684de87fc8dee9261b0
naibor <liznaibor.en@gmail.com>
2018-06-06 16:42:32 +0300

    adduser authentication

--- fun_flask.py
+++ fun_flask.py
@@ -1,12 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, make_response,jsonify
+from flask_restful import Api, Resource, reqparse, fields, marshal
+import config
+# enviroment configurations
+from flask_httpauth import HTTPBasicAuth
+
+
 app = Flask(__name__)
+api = Api(app)
+auth = HTTPBasicAuth()
 
+@auth.get_password
+def get_password(username):
+    if username == 'Naibor':
+        return 'jojo'
+    return None
 
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'message':'unauthorized access'}),403)
 
-@app.route("/")
-def hello():
-    return "Fun Flask"
-
-
+ 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.config.from_object(config.Development)
+    app.run()
